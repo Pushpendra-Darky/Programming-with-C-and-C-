84c11c8aa3c771cfa9a59ec43033fa35c15c08de
Pushpendra-Darky <pushpendra.assassin@gmail.com>
2023-11-09 19:04:28 +0530

    hello world

---
+++ cpp-training/hello-world/src/hello-world.cpp
@@ -0,0 +1,14 @@
+/*		Author:	Pushpendra
+ * 		Title: 	Hellow World.
+ */
+
+#include<iostream>
+using namespace std; //append prefix to standard iostream function
+
+int main() {
+
+	cout << "Hello\nC++" << endl; //endl = newline, cout = print, std= standard
+	cout << "World" << endl;
+
+	return 0;
+}
