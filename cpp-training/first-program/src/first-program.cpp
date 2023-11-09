591613e84fa80558c0cb58b2f7731352e5406dcf
Pushpendra-Darky <pushpendra.assassin@gmail.com>
2023-11-09 19:04:02 +0530

    first c++ program

---
+++ cpp-training/first-program/src/first-program.cpp
@@ -0,0 +1,15 @@
+/*		Author:	Pushpendra
+ * 		Title: 	C++ FIrst Program.
+ */
+
+#include<iostream>
+
+
+int main() {
+
+	std::cout << "Hello\n";
+	std::cout << "C++" << std::endl; //endl = newline, cout = print, std= standard
+	std::cout << "World" << std::endl;
+
+	return 0;
+}
